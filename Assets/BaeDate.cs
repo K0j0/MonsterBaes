3665b001b5ce78ec7b5834824c23df6bf1316b03
Kojo Kumah <info@kojokumah.com>
2015-01-25 13:21:29 -0800

    Tested going on date

---
+++ Assets/BaeDate.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaeDate : MonoBehaviour {
+	public GameObject Klulu;
+	public GameObject Daisy;
+	public GameObject Dan;
+	public GameObject Buzz;
+
+	public GameObject Klulu_Kiss;
+	public GameObject Daisy_Kiss;
+	public GameObject Dan_Kiss;
+	public GameObject Buzz_Kiss;
+
+	public void Kiss(Baes who){
+		switch(who){
+			case Baes.BUZZ:
+			break;
+			case Baes.DAN:
+			break;
+			case Baes.DAISY:
+			break;
+			case Baes.KULU:
+			break;
+		}
+	}
+
+	public void StartDate(Baes who){
+		Klulu.SetActive (false);
+		Daisy.SetActive (false);
+		Dan.SetActive (false);
+		Buzz.SetActive (false);
+
+		switch(who){
+			case Baes.BUZZ:
+				Buzz.SetActive (true);
+			break;
+			case Baes.DAN:
+				Dan.SetActive (true);
+			break;
+			case Baes.DAISY:
+				Daisy.SetActive (true);
+			break;
+			case Baes.KULU:
+				Klulu.SetActive (true);
+			break;
+		}
+	}
+}
